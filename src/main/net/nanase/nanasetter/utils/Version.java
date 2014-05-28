715368b38296e3b979a2c89fd5c9d6f01baaac62
Tomona Nanase <nanase@users.noreply.github.com>
2014-05-28 22:35:11 +0900

    VersionクラスにJavadocを追加

--- src/main/net/nanase/nanasetter/utils/Version.java
+++ src/main/net/nanase/nanasetter/utils/Version.java
@@ -31,35 +31,87 @@ import java.util.regex.Pattern;
  * Project: Nanasetter
  * Created by nanase on 14/05/18.
  */
+
+/**
+ * バージョンを表現し、比較するための機能を提供します。
+ *
+ * @author Tomona Nanase
+ * @since Nanasetter 0.1
+ */
 public class Version implements Comparable<Version> {
     private static final Pattern parsePattern;
 
+    /** メジャーバージョンを表す数値。 */
     public final int major;
+
+    /** マイナーバージョンを表す数値。 */
     public final int minor;
+
+    /** ビルド番号を表す数値。 */
     public final int build;
+
+    /** リビジョンを表す数値。 */
     public final int revision;
+
+    /** サフィックス（接尾辞）を表す文字列。 */
     public final String suffix;
 
     static {
         parsePattern = Pattern.compile("^(\\d+)(?:\\.(\\d+)(?:\\.(\\d+)(?:\\.(\\d+))?)?)?([^\\. ]\\S*)?$");
     }
 
+    /**
+     * メジャーバージョンを使用してインスタンスを初期化します。
+     *
+     * @param major メジャーバージョンを表す数値。
+     */
     public Version(int major) {
         this(major, -1, -1, -1, null);
     }
 
+    /**
+     * メジャーバージョンとマイナーバージョンを使用してインスタンスを初期化します。
+     *
+     * @param major メジャーバージョンを表す数値。
+     * @param minor マイナーバージョンを表す数値。
+     */
     public Version(int major, int minor) {
         this(major, minor, -1, -1, null);
     }
 
+    /**
+     * メジャーバージョン、マイナーバージョンおよびビルド番号を使用してインスタンスを初期化します。
+     *
+     * @param major メジャーバージョンを表す数値。
+     * @param minor マイナーバージョンを表す数値。
+     * @param build ビルド番号を表す数値。
+     */
     public Version(int major, int minor, int build) {
         this(major, minor, build, -1, null);
     }
 
+    /**
+     * メジャーバージョン、マイナーバージョン、ビルド番号およびリビジョンを使用してインスタンスを初期化します。
+     *
+     * @param major メジャーバージョンを表す数値。
+     * @param minor マイナーバージョンを表す数値。
+     * @param build ビルド番号を表す数値。
+     * @param revision リビジョンを表す数値。
+     */
     public Version(int major, int minor, int build, int revision) {
         this(major, minor, build, revision, null);
     }
 
+    /**
+     * バージョン表現に用いられるすべてのパラメータを使用してインスタンスを初期化します。
+     *
+     * @param major メジャーバージョンを表す数値。
+     * @param minor マイナーバージョンを表す数値。
+     * @param build ビルド番号を表す数値。
+     * @param revision リビジョンを表す数値。
+     * @param suffix サフィックスを表す文字列。
+     *               nullが与えられた場合は空の文字列として代用されます。
+     */
     public Version(int major, int minor, int build, int revision, String suffix) {
         this.major = major;
         this.minor = minor;
@@ -133,6 +185,13 @@ public class Version implements Comparable<Version> {
         return 0;
     }
 
+    /**
+     * 文字列で表現されたバージョンを解析し、新しい Version クラスのインスタンスを求めます。
+     *
+     * @param text 文字列で表現されたバージョン。
+     *             各バージョン番号はピリオド '.' で分離され、サフィックスはバージョン部分に続いて記述されます。
+     * @return 文字列を解析して得られた新しい Version クラスのインスタンス。
+     */
     public static Version parse(String text) {
         if (text == null)
             throw new IllegalArgumentException("null");
