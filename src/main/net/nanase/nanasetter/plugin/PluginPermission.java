cd1fc2ae73d118cc6384632cc5c20ebffb1b90de
Tomona Nanase <nanase@users.noreply.github.com>
2014-05-31 22:54:06 +0900

    PluginPermission列挙体にJavadocを追加

--- src/main/net/nanase/nanasetter/plugin/PluginPermission.java
+++ src/main/net/nanase/nanasetter/plugin/PluginPermission.java
@@ -30,13 +30,53 @@ import java.util.EnumSet;
  * Project: Nanasetter
  * Created by nanase on 14/05/15.
  */
+
+/**
+ * プラグインで利用できる Twitter 機能を区分するためのパーミッション(権限)を表現する列挙体です。
+ *
+ * @author Tomona Nanase
+ * @since Nanasetter 0.1
+ */
 public enum PluginPermission {
+    /**
+     * 読み取り権限。
+     * タイムラインの読み取り、リストの読み取り、ツイートの表示などが可能です。
+     */
     read,
+
+    /**
+     * ストリーム権限。
+     * ストリーム通知の受け取りが可能です。
+     */
     stream,
+
+    /**
+     * 書き込み権限。
+     * ツイート送信と削除、リツイート、お気に入り、リストの変更、プロフィールの変更などが可能です。
+     */
     write,
+
+    /**
+     * ダイレクトメッセージ権限。
+     * ダイレクトメッセージの読み込み、送信、削除が可能です。
+     */
     directMessage,
+
+    /**
+     * クリティカル権限。
+     * ブロック、フォローとフォロー解除などが可能です。
+     */
     critical,;
 
+    /**
+     * PluginPermission 列挙体に属する列挙値すべてを含んだ EnumSet オブジェクトを表します。
+     * この権限を持つプラグインはすべての Twitter 機能を使用します。
+     */
     public final static EnumSet<PluginPermission> full = EnumSet.allOf(PluginPermission.class);
+
+    /**
+     * PluginPermission 列挙体に属する列挙値の何れも含まない EnumSet オブジェクトを表します。
+     * この権限を持つプラグインは一切の Twitter 機能を使用しません。
+     */
     public final static EnumSet<PluginPermission> none = EnumSet.noneOf(PluginPermission.class);
 }
