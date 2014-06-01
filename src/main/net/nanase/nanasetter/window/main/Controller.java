92b8067af0124358a573d8adddd7beb758966ab3
Tomona Nanase <nanase@users.noreply.github.com>
2014-06-01 23:28:48 +0900

    mainウィンドウを追加

---
+++ src/main/net/nanase/nanasetter/window/main/Controller.java
@@ -0,0 +1,18 @@
+package net.nanase.nanasetter.window.main;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private WebView htmlRoot;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
