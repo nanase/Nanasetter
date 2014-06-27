4b6a12603a6a11d70dc212cf2311c9e4745be0de
Tomona Nanase <nanase@users.noreply.github.com>
2014-06-27 21:58:44 +0900

    DialogUtilsImpl.javaを追加

---
+++ src/main/net/nanase/nanasetter/window/dialog/DialogUtilsImpl.java
@@ -0,0 +1,105 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Tomona Nanase
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.nanase.nanasetter.window.dialog;
+
+import javafx.stage.Window;
+import net.nanase.nanasetter.utils.JSObjectUtils;
+import netscape.javascript.JSObject;
+import org.controlsfx.dialog.Dialogs;
+
+/**
+ * Project: Nanasetter
+ * Created by nanase on 14/06/27.
+ */
+
+class DialogUtilsImpl {
+    public static void showMessage(Window window, String message) {
+        if (message == null)
+            return;
+
+        Dialogs.create()
+                .owner(window)
+                .title("")
+                .message(message)
+                .showInformation();
+    }
+
+    public static void showMessage(Window window, JSObject parameters) {
+        if (parameters == null)
+            return;
+
+        Dialogs dialogs = Dialogs.create().owner(window).title("");
+
+        JSObjectUtils.ifExists(parameters, "message", String.class, dialogs::message);
+        JSObjectUtils.ifExists(parameters, "masthead", String.class, dialogs::masthead);
+        JSObjectUtils.ifExists(parameters, "title", String.class, dialogs::title);
+
+        if (JSObjectUtils.hasMember(parameters, "button"))
+            dialogs.actions(ClosingAction.parseFromJS(parameters));
+
+        String type = JSObjectUtils.getMember(parameters, "type", String.class).orElse("info");
+
+        switch (type) {
+            case "warning":
+            case "warn":
+                dialogs.showWarning();
+                break;
+
+            case "error":
+                dialogs.showError();
+                break;
+
+            default:
+                dialogs.showInformation();
+                break;
+        }
+    }
+
+    public static String confirm(Window window, String message) {
+        return Dialogs.create()
+                .owner(window)
+                .title("")
+                .message(message)
+                .showConfirm()
+                .toString()
+                .toLowerCase();
+    }
+
+    public static String confirm(Window window, JSObject parameters) {
+        if (parameters == null)
+            return null;
+
+        Dialogs dialogs = Dialogs.create().owner(window).title("");
+
+        JSObjectUtils.ifExists(parameters, "message", String.class, dialogs::message);
+        JSObjectUtils.ifExists(parameters, "masthead", String.class, dialogs::masthead);
+        JSObjectUtils.ifExists(parameters, "title", String.class, dialogs::title);
+
+        if (JSObjectUtils.hasMember(parameters, "button"))
+            dialogs.actions(ClosingAction.parseFromJS(parameters));
+
+        return dialogs.showConfirm().toString().toLowerCase();
+    }
+}
