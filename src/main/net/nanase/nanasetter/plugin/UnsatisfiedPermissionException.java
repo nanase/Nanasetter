4a5e2bc681eab53e6b06fa97a171936e1dca320b
Tomona Nanase <nanase@users.noreply.github.com>
2014-07-25 21:15:35 +0900

    UnsatisfiedPermissionException.javaを追加

---
+++ src/main/net/nanase/nanasetter/plugin/UnsatisfiedPermissionException.java
@@ -0,0 +1,47 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Tomona Nanase
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.nanase.nanasetter.plugin;
+
+/**
+ * Project: Nanasetter
+ * Created by nanase on 14/07/25.
+ */
+
+/**
+ * プラグインで指定されていないパーミッションを要求した時に発生する例外です。
+ *
+ * @author Tomona Nanase
+ * @since Nanasetter 0.1
+ */
+public class UnsatisfiedPermissionException extends Exception {
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public UnsatisfiedPermissionException(PluginPermission requestedPermission) {
+        super(String.format("プラグインで指定されていないパーミッション '%s' が要求されました。", requestedPermission.name()));
+    }
+}
