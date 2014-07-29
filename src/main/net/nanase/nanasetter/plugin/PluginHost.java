6373eb662f2941a7f1618d87c4d45a3aeab3c267
Tomona Nanase <nanase@users.noreply.github.com>
2014-07-29 19:06:23 +0900

    PluginHostクラスにJavadocを追加

--- src/main/net/nanase/nanasetter/plugin/PluginHost.java
+++ src/main/net/nanase/nanasetter/plugin/PluginHost.java
@@ -34,12 +34,25 @@ import java.util.logging.Logger;
  * Created by nanase on 14/06/01.
  */
 
+/**
+ * プラグインがななせったーにアクセスするための機能を提供します。
+ *
+ * @author Tomona Nanase
+ * @since Nanasetter 0.1
+ */
 public class PluginHost {
     private final Plugin plugin;
     private final Logger logger;
     private final TwitterList twitterList;
     private final Dialog dialog;
 
+    /**
+     * プラグインなどのパラメータを元に、新しい PluginHost クラスのインスタンスを初期化します。
+     *
+     * @param plugin      プラグイン。
+     * @param twitterList Twitter オブジェクトが格納された TwitterList オブジェクト。
+     * @param dialog      Dialog オブジェクト。
+     */
     public PluginHost(Plugin plugin, TwitterList twitterList, Dialog dialog) {
         if (plugin == null)
             throw new IllegalArgumentException();
@@ -56,46 +69,108 @@ public class PluginHost {
         this.dialog = dialog;
     }
 
+    /**
+     * READ_REST パーミッションで実行できる機能を提供するポーターオブジェクトを取得します。
+     *
+     * @return ReadRESTPorter オブジェクト。
+     * @throws UnsatisfiedPermissionException プラグインで指定されていないパーミッションが要求されました。
+     */
     public ReadRESTPorter getReadREST() throws UnsatisfiedPermissionException {
         return new ReadRESTPorter(this.twitterList, this);
     }
 
+    /**
+     * WRITE パーミッションで実行できる機能を提供するポーターオブジェクトを取得します。
+     *
+     * @return WritePorter オブジェクト。
+     * @throws UnsatisfiedPermissionException プラグインで指定されていないパーミッションが要求されました。
+     */
     public WritePorter getWrite() throws UnsatisfiedPermissionException {
         return new WritePorter(this.twitterList, this);
     }
 
+    /**
+     * READ_STREAMING パーミッションで実行できる機能を提供するポーターオブジェクトを取得します。
+     *
+     * @return ReadStreamingPorter オブジェクト。
+     * @throws UnsatisfiedPermissionException プラグインで指定されていないパーミッションが要求されました。
+     */
     public ReadStreamingPorter getReadStreaming() throws UnsatisfiedPermissionException {
         return new ReadStreamingPorter(this.twitterList, this);
     }
 
+    /**
+     * EXTEND パーミッションで実行できる機能を提供するポーターオブジェクトを取得します。
+     *
+     * @return ExtendPorter オブジェクト。
+     * @throws UnsatisfiedPermissionException プラグインで指定されていないパーミッションが要求されました。
+     */
     public ExtendPorter getExtend() throws UnsatisfiedPermissionException {
         return new ExtendPorter(this.twitterList, this);
     }
 
+    /**
+     * CONFIGURE パーミッションで実行できる機能を提供するポーターオブジェクトを取得します。
+     *
+     * @return ConfigurePorter オブジェクト。
+     * @throws UnsatisfiedPermissionException プラグインで指定されていないパーミッションが要求されました。
+     */
     public ConfigurePorter getConfigure() throws UnsatisfiedPermissionException {
         return new ConfigurePorter(this.twitterList, this);
     }
 
+    /**
+     * ACCESS_DIRECT_MESSAGE パーミッションで実行できる機能を提供するポーターオブジェクトを取得します。
+     *
+     * @return AccessDirectMessagePorter オブジェクト。
+     * @throws UnsatisfiedPermissionException プラグインで指定されていないパーミッションが要求されました。
+     */
     public AccessDirectMessagePorter getAccessDirectMessage() throws UnsatisfiedPermissionException {
         return new AccessDirectMessagePorter(this.twitterList, this);
     }
 
+    /**
+     * RISK パーミッションで実行できる機能を提供するポーターオブジェクトを取得します。
+     *
+     * @return RiskPorter オブジェクト。
+     * @throws UnsatisfiedPermissionException プラグインで指定されていないパーミッションが要求されました。
+     */
     public RiskPorter getRisk() throws UnsatisfiedPermissionException {
         return new RiskPorter(this.twitterList, this);
     }
 
+    /**
+     * Dialog オブジェクトを取得します。
+     *
+     * @return Dialog オブジェクト。
+     */
     public Dialog getDialog() {
         return this.dialog;
     }
 
+    /**
+     * Plugin オブジェクトを取得します。
+     *
+     * @return このプラグインホストに割り当てられた Plugin オブジェクト。
+     */
     public Plugin getPlugin() {
         return this.plugin;
     }
 
+    /**
+     * Logger オブジェクトを取得します。
+     *
+     * @return このプラグインホストに割り当てられた Logger オブジェクト。
+     */
     public Logger getLogger() {
         return this.logger;
     }
 
+    /**
+     * TwitterList オブジェクトを取得します。
+     *
+     * @return TwitterList オブジェクト。
+     */
     public TwitterList getTwitterList() {
         return this.twitterList;
     }
