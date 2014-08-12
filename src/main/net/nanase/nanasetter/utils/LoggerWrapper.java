29ceb719127ff03eafb1708c3667d687f657db03
Tomona Nanase <nanase@users.noreply.github.com>
2014-08-12 19:46:56 +0900

    LoggerWrapper.javaを追加

---
+++ src/main/net/nanase/nanasetter/utils/LoggerWrapper.java
@@ -0,0 +1,68 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Tomona Nanase
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.nanase.nanasetter.utils;
+
+import java.util.logging.Logger;
+
+/**
+ * Project: Nanasetter
+ * Created by nanase on 14/08/12.
+ */
+
+public class LoggerWrapper {
+    private final Logger logger;
+
+    public LoggerWrapper(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void config(String msg) {
+        // stub
+    }
+
+    public void fine(String msg) {
+        // stub
+    }
+
+    public void finer(String msg) {
+        // stub
+    }
+
+    public void finest(String msg) {
+        // stub
+    }
+
+    public void info(String msg) {
+        // stub
+    }
+
+    public void severe(String msg) {
+        // stub
+    }
+
+    public void warning(String msg) {
+        // stub
+    }
+}
